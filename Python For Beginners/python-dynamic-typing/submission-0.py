1fda0fa4441444a333493923909770c40657899b
Carl Coleman <carbolizer3@gmail.com>
2026-08-08 13:51:27 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,11 @@
+variable = 10
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "bro"
+print(type(variable))
+variable = [1,3]
+print(type(variable))
+
